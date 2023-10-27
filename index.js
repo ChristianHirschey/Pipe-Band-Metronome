6acdc1ef4d0273ea3a0618626957ca7db9be28fc
ChristianHirschey <christian.hirschey@gmail.com>
2023-10-27 22:56:34 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import playMetronome from './metronome';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
